0df95f779bb1986c8ef0b1dfd2800a69e21b82a8
RitikRajawat07 <ritikrajawat10@gmail.com>
2024-06-13 16:26:24 +0530

    Till_Lec-3

--- LEC-1,2/test.js
+++ LEC-1,2/test.js
@@ -58,14 +58,46 @@ practice :-
 // root2.render(parent);
 
 
-L-3
+                           // *! L-3 part-1
 
-const jsxHeading=(
+// const jsxHeading=(
+//     <h1 className="head" tabIndex="5">
+//         Namaste React 
+//     </h1>
+// );
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
+
+                           // *! part2 lec3
+
+// JSX or React Element
+
+const heading=(
     <h1 className="head" tabIndex="5">
         Namaste React 
     </h1>
 );
 
+// React Component
+
+const Title = () => (
+    <h1 className="title">
+        Namaste React  
+    </h1>
+);
+
+// Component Composition -- using one component in other. 
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title/> 
+        {/* or <Title></Title> or {Title()} */}
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
